0e171910971c024c39b88f7875f8c5b17bd5c516
Ramazan Doğan <doganrmzn40@gmail.com>
2023-07-22 09:42:34 +0300

    Animated

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -3,83 +3,92 @@ import Pokepoke from '../../assets/images/pokepoke-app.png';
 import React from 'react';
 import ReactPortfolio from '../../assets/images/react-portfolio.png';
 import Umutvadi from '../../assets/images/umutvadi.png';
+import { motion } from 'framer-motion';
 import styles from '../../assets/styles/projects.module.css';
+const projectsData = {
+   projects: [
+      {
+         title: 'Pokepoke App',
+         image: Pokepoke,
+         description: 'Pokedex app with PokeAPI. (React, Redux Toolkit, CSS)',
+         url: 'https://pokepokeapp.netlify.app/',
+      },
+      {
+         title: 'Umutvadi',
+         image: Umutvadi,
+         description:
+            'My personal website - 1.5M Single hit (Wordpress, Content Writer, SEO)',
+         url: 'https://umutvadi.com/',
+      },
+      {
+         title: 'MovieAPI',
+         image: MovieApp,
+         description: 'Movie watchlist app. (React, Redux Toolkit, MaterialUI)',
+         url: 'https://movie-api-app-demo.netlify.app',
+      },
+      {
+         title: 'Portfolio',
+         image: ReactPortfolio,
+         description:
+            'A simple portfolio web app. (React, React Hooks, TailwindCSS)',
+         url: 'https://react-portfoilo-demo.netlify.app',
+      },
+   ],
+};
+
+const container = {
+   visible: {
+      transition: {
+         delayChildren: 0.3,
+         staggerChildren: 0.1,
+      },
+   },
+};
+
+const item = {
+   hidden: {
+      opacity: 0,
+      translateY: 30,
+   },
+
+   visible: {
+      opacity: 1,
+      translateY: 0,
+   },
+};
 
 function ProjectCard() {
    return (
-      <div className={styles.projects}>
-         <div className={styles.photoContainer}>
-            <a
-               className={styles.projectImageContainer}
-               target="_blank"
-               rel="noreferrer"
-               href="https://pokepokeapp.netlify.app/"
+      <motion.div
+         variants={container}
+         initial="hidden"
+         animate="visible"
+         transition={{ delay: 0.05 }}
+         className={styles.projects}
+      >
+         {projectsData.projects.map((project, index) => (
+            <motion.div
+               variants={item}
+               className={styles.photoContainer}
+               key={index}
             >
-               <img
-                  className={styles.projectImage}
-                  src={Pokepoke}
-                  alt=""
-               />
-               <p className={styles.projectP}>Pokepoke App</p>
-               <p className={styles.projectP2}>
-                  Pokedex app with PokeAPI. <br /> (React, Redux Toolkit, CSS)
-               </p>
-            </a>
-            <a
-               className={styles.projectImageContainer}
-               target="_blank"
-               rel="noreferrer"
-               href="https://umutvadi.com/"
-            >
-               <img
-                  className={styles.projectImage}
-                  src={Umutvadi}
-                  alt=""
-               />
-               <p className={styles.projectP}>Umutvadi</p>
-               <p className={styles.projectP2}>
-                  My personal website - 1.5M Single hit
-                  <br /> (Wordpress, Content Writer, SEO)
-               </p>
-            </a>
-         </div>
-         <div className={styles.photoContainer}>
-            <a
-               className={styles.projectImageContainer}
-               target="_blank"
-               rel="noreferrer"
-               href="https://movie-api-app-demo.netlify.app"
-            >
-               <img
-                  className={styles.projectImage}
-                  src={MovieApp}
-                  alt=""
-               />
-               <p className={styles.projectP}>MovieAPI</p>
-               <p className={styles.projectP2}>
-                  Movie watchlist app.
-                  <br /> (React, Redux Toolkit, MaterialUI)
-               </p>
-            </a>
-            <a
-               className={styles.projectImageContainer}
-               target="_blank"
-               rel="noreferrer"
-               href="https://react-portfoilo-demo.netlify.app"
-            >
-               <img
-                  className={styles.projectImage}
-                  src={ReactPortfolio}
-                  alt=""
-               />
-               <p className={styles.projectP}>Portfolio</p>
-               <p className={styles.projectP2}>
-                  A simple portfolio web app.
-                  <br /> (React, React Hooks, TailwindCSS)
-               </p>
-            </a>
-         </div>
-      </div>
+               <a
+                  className={styles.projectImageContainer}
+                  target="_blank"
+                  rel="noreferrer"
+                  href={project.url}
+               >
+                  <img
+                     className={styles.projectImage}
+                     src={project.image}
+                     alt={project.title}
+                  />
+                  <p className={styles.projectP}>{project.title}</p>
+                  <p className={styles.projectP2}>{project.description}</p>
+               </a>
+            </motion.div>
+         ))}
+      </motion.div>
    );
 }
 
